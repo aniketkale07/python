0a87374bb704d33ea7656b455ed70f5108565e55
Aniket Kale <164205726+aniketkale07@users.noreply.github.com>
2024-07-18 12:38:44 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,20 @@
+# for i in range(3):
+#     # print value of i upto 3
+#     print(i)    
+    
+    
+    
+# # using statment break and continue
+# for i in range(13):
+#     if(i==3):
+#         continue
+    
+#     if(i==5):
+#         break
+#     print(i)
+        
+        
+# Multiplication Table 
+
+for i in range(1,11):
+    print(i*2)
